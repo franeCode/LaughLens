c0042cce4afb575fc163a4a73080bdb9c4ae4935
Ana Popovic <franjetina@gmail.com>
2023-09-06 09:53:53 +0200

    form styled

--- feed/forms.py
+++ feed/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 class PostForm(forms.Form):
+    title = forms.CharField(label="Title")
+    description = forms.CharField(widget=forms.Textarea)
     image = forms.FileField()
-    text = forms.CharField(label="Description")
     
