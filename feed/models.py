3a4317c984268437fde74c9b7edf9e44a06e9069
Ana Popovic <franjetina@gmail.com>
2023-09-06 09:53:36 +0200

    description field added, form styled

--- feed/models.py
+++ feed/models.py
@@ -3,7 +3,8 @@ from sorl.thumbnail import ImageField
 
 
 class Post(models.Model):
-    text = models.CharField(max_length=140, blank=False, null=False)
+    title = models.CharField(max_length=140, blank=False, null=False)
+    description = models.TextField(blank = True, null=False)
     image = ImageField()
     def __str__(self):
         return self.text
